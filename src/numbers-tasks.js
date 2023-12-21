50ca4b44555921931ba81e01ac4f6f67b1ecf2c0
Ivan Pakhomov <ip1133@ya.ru>
2023-12-21 21:36:40 +0300

    feat: feat 17 tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -296,6 +296,7 @@ function getSumToN(n) {
     arr.push(i);
     i += 1;
   }
+  // console.log(arr)
   return arr.reduce((a, b) => a + b, 0);
 }
 
@@ -310,8 +311,9 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const arr = Array.from(String(num), Number);
+  return arr.reduce((a, b) => a + b, 0);
 }
 
 /**
